96ae9bbf47223dab81084c9bc2d2f46e93c8b73f
“Msofyan14” <mxsofyan14@gmail.com>
2023-01-09 03:32:02 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "assets/scss/style.scss";
+
+function App() {
+  return (
+    <div>
+      {/* <header className="App-header">
+        <h1 className="btn btn-warning">TESSSSSSS</h1>
+        <h1 className="btn btn-warning">TESSSSSSS</h1>
+        <h1 className="btn btn-warning">TESSSSSSS</h1>
+        <h1 className="btn btn-danger">TESSSSSSS</h1>
+        <h1 className="btn btn-primary">ok</h1>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
